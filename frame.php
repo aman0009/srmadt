dac55e1b329ad5c21043b5d8b47dd1fb11f36600
hellloamman <amansharma33.as@gmail.com>
2017-08-22 20:17:26 +0530

    adt

---
+++ frame.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="aman.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+</head>
+
+<body bgcolor="#f2f4f6">
+
+<table width="457" border="1">
+  <tbody>
+    <tr>
+      <td width="120"><div class="box"><i class="material-icons">info</i></div></td>
+      <td width="120"><div class="box"><i class="material-icons">mode_comment</i></div></td>
+      <td width="120"><div class="box">&nbsp;</div></td>
+      <td width="70"><div class="box"><i class="material-icons">close</i></div></td>
+    </tr>
+  </tbody>
+</table>
+
+    <div class="post__sidebar-header-heading clearfix">
+      <p>
+        <img class="avatar" width="48" height="48" alt="6f45be4b588210ef10c40ae3e566d591 normal" src=
+        "https://pbs.twimg.com/profile_images/378800000853902009/6f45be4b588210ef10c40ae3e566d591_normal.jpeg">
+        <span style="font-size:24px">YouTube Redesign II</span>
+      </p>
+      <p>
+        <span>by</span>
+        <a href="/marcus_hofer">Marcus Hofer</a>
+        <span>in</span>
+        <a href="http://www.uplabs.com/posts/c/resources/website">Websites</a>
+        <span>for</span>
+        <a href="/platforms/web">Web</a>
+        <a class="btn btn-follow" login="" href="/login">Follow </a>
+      </p>
+    </div>
+
+    
+<table width="1149" border="1" bordercolor="#B7B7B7">
+  <tbody>
+   <tr>
+      <td width="1070" height="70" style="color:#8A8A8A; font-weight:700">Made with 
+      </td>
+      <td width="63">
+          <img src=
+             "http://www.material.uplabs.com/assets/tools/photoshop-0d4810b9a477ebe250d0b8ad03c16dd1abdf2e4e36ff286a8bbeba3ab237993e.png"          alt="Photoshop" width="44" height="42">
+           <div class="tool__list-item-inner clearfix">
+           <span></span>
+          </div>
+      </td>
+  </tr>
+   
+ 
+   <tr>
+      <td height="70" style="color:#8A8A8A; font-weight:700">
+       Showcased on<a href="http://www.material.uplabs.com"> MaterialUp  </a>
+      </td>
+      <td> 
+       <data-original-title="MaterialUp" href="http://www.material.uplabs.com" tooltip="" title="" data-placement="left">
+       <a><img src=   "http://www.material.uplabs.com/assets/logo/materialup/default--128x128-46d99c52b3922ae01d2bd1f3f9dd9c6728db366bcfb2716792674dbe6528784d.png" alt="Default  128x128" width="44" height="42"></a>
+      <div>
+      </div>
+      </td>
+  </tr>
+    <tr>
+    <td> View on Dribbble</td>
+    </tr>
+  </tbody>
+</table>
+
+<table width="10" border="1">
+  <tbody>
+    <tr>
+      <td height="154"><img src="vignette.png" width="177" height="135"></td>
+      <td><img src="vignette1.png" width="170" height="134"></td>
+      <td><img src="vignette3.png" width="170" height="133"></td>
+    </tr>
+  </tbody>
+</table>
+<p>&nbsp;</p>
+<table width="10" border="1">
+  <tbody>
+    <tr>
+      <td height="154"><img src="vignette4.png" width="177" height="135"></td>
+      <td><img src="vignette5.png" width="170" height="134"></td>
+      <td><img src="vignette6.png" width="170" height="133"></td>
+    </tr>
+  </tbody>
+</table>
+<table width="10" border="1">
+  <tbody>
+    <tr>
+      <td height="154"><img src="vignette7.png" width="177" height="135"></td>
+      <td><img src="vignette8.png" width="170" height="134"></td>
+      <td><img src="vignette9.png" width="170" height="133"></td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
---
+++ srm.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="light--128x128-af4188d81df0291400f4cc7a5ae090c2c62870bc7481f158ada53f3540f681ce.png"  sizes="192x192" rel="icon">
+<title>Youtube Redesign ii</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+</head>
+<body>
+<div class="a" style="widows:100%; height:100%">
+<table width="99%" border="1">
+      <tr>
+         <td width="6%" height="69">
+          <a class="ham"><i class="material-icons">menu</i></a>
+         </td>
+         <td width="4%">
+           <div class="logo">
+           <img src="default--small-0849a93602df0e8708cfd15836f1ae21a938272dddc0c51eeb5466d1d326dc9a.png" style="height:32px; width:30px">
+ </div>
+         </td>
+         <td width="4%">
+            <img src="light--128x128-af4188d81df0291400f4cc7a5ae090c2c62870bc7481f158ada53f3540f681ce.png" alt="aman" width="28px" height="23px">
+          </td>
+          <td width="24%" style="font-size:20px; font-family:Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif">
+              material<b>up</b>
+          </td>
+          <td width="58%" >
+            <div class="search">
+            <input style="width:80%; height:40px; background-color: #4A44D0" type="search" placeholder="Search for material design   apps,icons, tools & more…">
+            </div>
+          </td>
+          <td width="2%">
+             <div class="b">
+             <i class="material-icons">add_circle_outline</i>
+             </div>
+         </td>
+          <td width="2%">LOGIN</td>
+   </tr>
+</table>
+</div>
+
+<div class="tool">
+<table width="947" border="1">
+    <tr>
+      <div class="too">
+      <td width="456">&nbsp;</td>
+      <td width="141"> SHOWCASE</td>
+      <td width="157">DESCRIPTION</td>
+      <td width="165">INTERNSHIPS</td>
+      </div>
+    </tr>
+</table>
+</div>
+
+<div class="progress">
+<div class="indeterminate"></div>
+</div>
+<div class="img">
+ <iframe height="590px" width="830px" src="unbenannt-1-wiederhergestellt.jpg">
+         <div class="card-panel">
+        <button class="btn waves-effect waves-teal"><i class="material-icons left">add</i>Add</button>
+         </div>
+         
+ </iframe>
+ <iframe height="590px" width="428px" src="frame.php">
+ </iframe>
+ 
+ 
+ <div class="save">
+    <a><button class="btn waves-effect waves-teal"><i class="material-icons left">add</i>SAVE</button></a>
+     <a><button class="btn btn-secondary btn-icon">
+      <i class="material-icons">share</i></button>
+     </a>
+     <a class="btn btn-secondary btn-icon">
+     <i class="material-icons">file_download</i>
+     </a>
+</div>
+
+</div>  
+</body>
+</html>
